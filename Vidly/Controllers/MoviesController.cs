da365d028021a36a11da0807a37b1bfc8573a869
Saurav Neupane <srvneupane@gmail.com>
2021-01-30 21:59:27 -0600

    Add project files.

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 2"},
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 2"},
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 2"},
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+
+        }
+
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie {Id = 1, Name="Aj de"},
+                new Movie {Id = 2, Name="Rang De Basanti"},
+            };
+        }
+    }
+}
